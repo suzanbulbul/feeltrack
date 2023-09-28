1661a410f90b495e2dce35b570b1feb35ce33f5a
Your Name <you@example.com>
2023-09-28 20:49:28 +0300

    Dev: added user-editor area

--- components/header/index.jsx
+++ components/header/index.jsx
@@ -1,4 +1,4 @@
-import React  from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useRouter } from 'next/router';
 import { useDispatch } from 'react-redux';
 import Link from 'next/link';
@@ -6,18 +6,36 @@ import Link from 'next/link';
 // Firebase
 import { logout } from "../../utilities/firebase";
 // Store
-import { logout as logoutHandle, logoutComplete} from "../../redux/userSlice";
+import { logout as logoutHandle, logoutComplete } from "../../redux/userSlice";
 
-//Icons
+// Icons
 import { BiUserCircle } from "react-icons/bi";
 import { MdAutoGraph } from "react-icons/md";
 
 const Header = () => {
   const router = useRouter();
   const dispatch = useDispatch();
+  const [userEditor, setUserEditor] = useState(false);
+
+  const userEditorRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (userEditorRef.current && !userEditorRef.current.contains(event.target)) {
+        setUserEditor(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   const handleLogout = async () => {
     try {
+      setUserEditor(false);
+
       dispatch(logoutHandle());
       await logout();
       dispatch(logoutComplete());
@@ -29,26 +47,43 @@ const Header = () => {
     }
   };
 
+  const routeringPage = (page) => () => {
+    setUserEditor(false);
+    router.push(page);
+  }
+
   return (
     <div className="header">
       <div className="flex justify-between items-center">
-        <span></span>
-        <Link href="/home">
+        <Link className="logo" href="/home">
           <img
-            className="logo"
             src="https://firebasestorage.googleapis.com/v0/b/feel-track.appspot.com/o/logo-white.svg?alt=media&token=854088fa-f9fc-4a83-a965-e22f77b7daa2"
             alt="logo"
           />
         </Link>
         <div className="flex justify-between items-center">
           <Link href="/analysis">
-            <MdAutoGraph className="icon"/>
+            <MdAutoGraph className="icon" />
           </Link>
-          <BiUserCircle className="icon"/>
+          <div ref={userEditorRef}>
+            <BiUserCircle onClick={() => setUserEditor(!userEditor)} className={`icon ${userEditor && `active`}`} />
+            {userEditor && (
+              <div className="user-editor">
+                <ul>
+                  <li>
+                    <button onClick={routeringPage("/analysis")}>Analysis</button>
+                  </li>
+                  <li>
+                    <button onClick={routeringPage("/settings")}>Settings</button>
+                  </li>
+                  <li>
+                    <button onClick={handleLogout}>Çıkış</button>
+                  </li>
+                </ul>
+              </div>
+            )}
+          </div>
         </div>
-      </div>
-      <div>
-            <button onClick={handleLogout}>Çıkış</button>
       </div>
     </div>
   );
