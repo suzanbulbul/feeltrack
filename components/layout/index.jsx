dce9d9c3079bafb69b05bc39dbae4c456860c7e0
Suzan Bülbül <suzanbulbl@gmail.com>
2023-10-07 14:52:38 +0300

    Fix: non-user layout error

--- components/layout/index.jsx
+++ components/layout/index.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { useRouter } from 'next/router'; 
+
+// Redux
+import { selectUser } from '../../redux/userSlice';
 
 // Components
 import Header from '../header';
 
 const Layout = ({ children }) => {
+  const user = useSelector(selectUser);
+  console.log(user);
 
   const router = useRouter();
 
-  if (router.pathname === '/' || router.pathname === '/404' ) {
+  if (router.pathname === '/' || router.pathname === '/404' || !user) {
     return <main>{children}</main>;
   }
 
